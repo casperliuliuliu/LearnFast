fd96e774661fcdabf408686f2d8c56e10a3fafb4
Casperliuliuliu <casperliu1118@gmail.com>
2023-08-20 15:32:48 +0800

    adding selecting functions

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import random
+import pandas as pd
 
 def pick_random(number_list: list=[], num: int=5)->list:
     # Check if the list has at least 5 unique elements
@@ -8,3 +9,65 @@ def pick_random(number_list: list=[], num: int=5)->list:
     else:
         print("The list doesn't have enough unique elements.")
         return []
+
+def vocab(number_list, df):
+    return df.iloc[number_list].reset_index(drop=1)
+
+def spelling_test(number_list):
+    test_list = pick_random(number_list)
+    # print(test_list)
+    test_df = vocab(test_list, df)
+    score = 0
+    wrong_ans = []
+    for ii in range(test_df.shape[0]):
+        print(test_df['chinese'][ii])
+        ans = input('input:')
+        if ans == test_df['vocabulary'][ii]:
+            score+=1
+        else:
+            wrong_ans.append(ii)
+        # print(test_df.iloc[ii])
+    print(f"{score}/{test_df.shape[0]}")
+    print(df.iloc[wrong_ans])
+
+def selection_test(df,number_list):
+    test_list = pick_random(number_list)
+    print(test_list)
+    test_df = vocab(test_list, df)
+    score = 0
+    ans = []
+    ans_list = []
+    print(test_df)
+    for ii in range(test_df.shape[0]):
+        print(f"\n[ {test_df['vocabulary'][ii]} ]")
+        without_list = list(range(1,test_list[ii])) + list(range(test_list[ii]+1,df.shape[0]))
+        random_list = pick_random(without_list,3)
+        random_list.append(test_list[ii])
+        random.shuffle(random_list)
+        ans_list.append(random_list.index(test_list[ii]))
+        # print(random_list)
+        for jj in range(4):
+
+            word = df['chinese'][random_list[jj]]
+            print(f"{jj}.{word}")
+        ans.append(input("輸入選項："))
+        if int(ans[ii]) == ans_list[ii]:
+            score += 1
+            print("Correct!")
+        else:
+            print("NO NO.")
+        # print(ans[ii], ans_list[ii])
+
+    print(f"YOUR SCORE: [ {score}/{test_df.shape[0]} ]")
+
+if __name__ == "__main__":
+    df = pd.read_excel('mason2000.xlsx')
+    print(df.head())
+    number_list = list(range(1,df.shape[0]))
+    selection_test(df,number_list)
+
+
+
+    
+
+    
